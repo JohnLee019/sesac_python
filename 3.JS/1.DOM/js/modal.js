8851926748afe92eab540a174f507b83ac092cbd
John Lee <ohno0601111@gmail.com>
2025-06-24 15:41:22 +0900

    새로운 학습 내용들

---
+++ 3.JS/1.DOM/js/modal.js
@@ -0,0 +1,12 @@
+const open = document.getElementById('open');
+const close = document.getElementById('close');
+const modal = document.querySelector('.modal-wrapper');
+//const modal = document.getElementsByClassName('.modal-wrapper')[0];
+
+open.onclick = () => {
+    modal.style.display = 'flex';
+}
+
+close.onclick = () => {
+    modal.style.display = 'none';
+}
---
+++ 3.JS/1.DOM/js/modal2.js
@@ -0,0 +1,37 @@
+const open = document.getElementById('open');
+
+open.onclick = () => {
+   showModal();
+}
+
+function showModal(){
+    //모달창 껍데기(div) 만들기
+    const modalWrapper = document.createElement('div');
+    modalWrapper.className = 'modal-wrapper';
+
+    //다 하나하나 만들기 귀찮아서, 모달창 안에 내용을 html로 채워넣기
+    modalWrapper.innerHTML = `
+    <div class="modal-wrapper" style="display: none">
+        <div class="modal">
+            <div class="modal-title">모달 타이틀</div>
+            <p>모달 본문 내용</p>
+            <div class="close-wrapper">
+                <button id="close">닫기 버튼</button>
+            </div>
+        </div>
+    </div>
+    `;
+
+    //body에다가 위에 만든 모달창 껍데기를 붙이기
+    document.body.appendChild(modalWrapper);
+
+    //닫기 버튼 이벤트 추가
+    // const close = document.getElementById('close');
+    // close.onclick = () => {
+    //     modalWrapper.remove();
+    // }
+    document.getElementById('close').onclick = () => {
+        modalWrapper.remove();
+    }
+}
+
