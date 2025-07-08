1198c054db190f79d3876fcae8c2828e7cf68e0c
John Lee <ohno0601111@gmail.com>
2025-07-08 17:52:56 +0900

    flask 이론

--- 6.Flask/2.templates/1.app.py
+++ 6.Flask/2.templates/1.app.py
@@ -7,4 +7,5 @@ def home():
     return render_template('index.html') # 이때 이 파일은 무조건 templates 라는 폴더 안에 있어야 함.
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+    app.run(host="0.0.0.0", debug=True)
---
+++ 6.Flask/2.templates/9.app_nameagephone.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+users = [
+    {'name': 'Alice', 'age': 25, 'mobile': '050-1234-5678'},
+    {'name': 'Alice', 'age': 35, 'mobile': '050-5555-5678'},
+    {'name': 'Bob', 'age': 30, 'mobile': '050-2222-5678'},
+    {'name': 'Charlie', 'age': 35, 'mobile': '050-3333-5678'},
+    {'name': 'David', 'age': 30, 'mobile': '050-4444-5678'},
+]
+
+# 미션1. 사용자 목록을 테이블로 그린다
+# <table border="1"> <tr> <td>
+# 미션2. 입력폼을 하나 만들고, 사용자 이름으로 원하는 사용자만 골라낸다
+
+# http://localhost:5000/?name=aaa&age=22
+
+@app.route('/')  # GET 파라미터 요청이 함께 온다는 것...
+def home():
+    name = request.args.get('name')
+    age = request.args.get('age')
+    phone = request.args.get('phone')
+
+    print(f"이름: {name}, 나이: {age}, 전화번호: {phone}")
+    filtered_users = users
+    
+    if name:
+        filtered_users = [u for u in filtered_users if name.lower() == u['name'].lower()]
+
+    if age:
+        filtered_users = [u for u in filtered_users if int(age) == u['age']]
+
+    if phone:
+        filtered_users = [u for u in filtered_users if phone in u['mobile']]
+
+    return render_template('index9.html', users=filtered_users)
+
+if __name__ == '__main__':
+    app.run(debug=True)
