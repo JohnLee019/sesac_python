91adfcca4eabce9bbadd41e0328dfcfccd91423e
John Lee <ohno0601111@gmail.com>
2025-07-16 14:25:13 +0900

    orm 학습

--- 4.Python/21.database_orm/3.flask.app/model.py
+++ 4.Python/21.database_orm/3.flask.app/model.py
@@ -7,3 +7,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     age = db.Column(db.Integer)
+
+    #객체를 print로 출력할때의 포멧 정의하는 함수
+    def __repr__(self):
+        return f'[User {self.id}: {self.name}, {self.age}]'
