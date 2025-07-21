96323f38a8d2156aabc9e00a4890eabd88fbc42a
John Lee <ohno0601111@gmail.com>
2025-07-21 15:25:02 +0900

    월 별로 각 아이템의 구매 사항 추가

---
+++ homework/crm/route/detail/item_detail.py
@@ -0,0 +1,13 @@
+from flask import Blueprint, render_template, request
+import database as db 
+
+item_detail_bp = Blueprint('item_detail', __name__, template_folder='../templates')
+
+@item_detail_bp.route('/item_detail')
+def order_detail():
+    item_id = request.args.get('id')
+    item = db.get_item_by_id(item_id)
+    unit_price = int(item[0]['UnitPrice'])
+    monthly_count = db.get_item_monthly_count(item_id, unit_price)
+    print(monthly_count)
+    return render_template('detail/item_detail.html', item=item, monthly_count=monthly_count)
