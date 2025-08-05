a2346377f0f0c934a94946c1bafb0f582f7a8b76
John Lee <ohno0601111@gmail.com>
2025-08-05 10:02:24 +0900

    gmail 수신

---
+++ homework/mail/4.cli_chatbot.py
@@ -0,0 +1,35 @@
+import openai
+from dotenv import load_dotenv
+import os
+from flask import Flask
+
+load_dotenv()
+
+
+client = openai.OpenAI()
+
+history = []
+
+def ask_chatgpt(user_input):
+    gpt_question = {'role':'user', 'content': user_input}
+    history.append(gpt_question)
+    print('실제 질문')
+    print(history)
+    print('-----------')
+
+    response = client.chat.completions.create(
+        model = 'gpt-3.5-turbo',
+        messages = history
+    )
+    gpt_response = {'role': 'ai', 'content': response.choices[0].message.content}
+    history.append(gpt_response)
+
+    return gpt_response['content']
+
+
+while True:
+    user_input = input("[YOU]: ")
+    if user_input in {"exit", "quit", "종료", "그만"}:
+        print("대화를 종료합니다")
+        break
+    print("[챗봇 응답]:", ask_chatgpt(user_input))
